c7f6b5125b28590da086abefc702417fb2bfb527
Sinju-Varghese <sinjuvarghese58@gmail.com>
2025-07-17 21:28:35 +0530

    CORS ADDED

--- ContactCloud/ContactCloud.Common/Types/Result.cs
+++ ContactCloud/ContactCloud.Common/Types/Result.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ContactCloud.Common.Types;
 
-namespace ContactCloud.Common.Types
+public class Result
 {
-    internal class Result
-    {
-    }
 }
--- ContactCloud/ContactCloud.Common/Types/ResultTypes.cs
+++ ContactCloud/ContactCloud.Common/Types/ResultTypes.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ContactCloud.Common.Types;
 
-namespace ContactCloud.Common.Types
+public class ResultTypes
 {
-    internal class ResultTypes
-    {
-    }
 }
